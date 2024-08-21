30d8e4bd888b63e304797f8c6628f4437954b0ea
amotor95 <amotor95@DESKTOP-Q87TU17>
2024-08-21 03:04:20 -0400

    Fixed infinite scroll home page

--- front-end/glass-table-app/src/components/Card.jsx
+++ front-end/glass-table-app/src/components/Card.jsx
@@ -14,4 +14,30 @@ function Card({imgSrc, cardName, cardPrice}) {
     );
 }
 
-export default Card;
+// export default Card;
+
+// export const Card = ({
+//     imgSrc,
+//     imgAlt,
+//     title,
+//     description,
+//     buttonText,
+//     link,
+//   }) => {
+//     return (
+//       <div className="card-container">
+//         {imgSrc && imgAlt && (
+//           <img src={imgSrc} alt={imgAlt} className="card-img" />
+//         )}
+//         {title && <h1 className="card-title">{title}</h1>}
+//         {description && <p className="card-description">{description}</p>}
+//         {buttonText && link && (
+//           <a href={link} className="card-btn">
+//             {buttonText}
+//           </a>
+//         )}
+//       </div>
+//     );
+//   };
+
+export default Card
--- front-end/glass-table-app/src/components/Home.jsx
+++ front-end/glass-table-app/src/components/Home.jsx
@@ -1,9 +1,62 @@
 import React from 'react';
 import './Home.css'
+import Card from './Card.jsx'
 
 export function Home() {
     return(
-        <div>This is the temporary home!</div>
+        <div className="home-screen">
+            <div className="row-container">
+                <div className="row">
+                    <Card className="item"></Card>
+                    <Card className="item"></Card>
+                    <Card className="item"></Card>
+                    <Card className="item"></Card>
+                    <Card className="item"></Card>
+                    <Card className="item"></Card>
+                    <Card className="item"></Card>
+                    <Card className="item"></Card>
+                    <Card className="item"></Card>
+                </div>
+                <div className="row" aria-hidden="true">
+                <Card className="item"></Card>
+                    <Card className="item"></Card>
+                    <Card className="item"></Card>
+                    <Card className="item"></Card>
+                    <Card className="item"></Card>
+                    <Card className="item"></Card>
+                    <Card className="item"></Card>
+                    <Card className="item"></Card>
+                    <Card className="item"></Card>
+                </div>
+            </div>
+            <div className="row-container">
+                <div className="row">
+                    <Card className="item"></Card>
+                    <Card className="item"></Card>
+                    <Card className="item"></Card>
+                    <Card className="item"></Card>
+                    <Card className="item"></Card>
+                    <Card className="item"></Card>
+                    <Card className="item"></Card>
+                    <Card className="item"></Card>
+                    <Card className="item"></Card>
+                </div>
+                <div className="row" aria-hidden="true">
+                <Card className="item"></Card>
+                    <Card className="item">vvv</Card>
+                    <Card className="item"></Card>
+                    <Card className="item"></Card>
+                    <Card className="item"></Card>
+                    <Card className="item"></Card>
+                    <Card className="item"></Card>
+                    <Card className="item"></Card>
+                    <Card className="item"></Card>
+                </div>
+            </div>
+        </div>
+
+
+        
     )
 }
 
