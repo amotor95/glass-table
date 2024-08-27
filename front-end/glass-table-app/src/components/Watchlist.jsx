8a42aa106271518ff8aac606ce3234678d6a4de6
Maanas Pavuluri <maanaspavuluri@Maanass-MacBook-Pro-2.local>
2024-08-27 11:32:17 -0400

    added dropdown

--- front-end/glass-table-app/src/components/Watchlist.jsx
+++ front-end/glass-table-app/src/components/Watchlist.jsx
@@ -6,12 +6,45 @@ import { UserContext } from '../context/UserContext.jsx';
 import { FuncContext } from '../context/FuncContext.jsx';
 
 
+
 function Watchlist() {
+    const myFunction = () => {
+        document.getElementById("myDropdown").classList.toggle("show");
+        console.log("hit buttom");
+    }
+    function filterFunction() {
+        var input, filter, div, a, i;
+        input = document.getElementById("myInput");
+        filter = input.value.toUpperCase();
+        div = document.getElementById("myDropdown");
+        a = div.getElementsByTagName("a");
+
+        for (i = 0; i < a.length; i++) {
+            var txtValue = a[i].textContent || a[i].innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                a[i].style.display = ""; // Show the item
+            } else {
+                a[i].style.display = "none"; // Hide the item
+            }
+        }
+    }
     
     return(
         <div className='watchlist-screen'>
             <div className='watchlist-topbar'>
-                
+                <div className="dropdown">
+                    <button onClick={myFunction} className="dropbtn">Dropdown</button>
+                    <div id="myDropdown" className="dropdown-content">
+                        <input type="text" placeholder="Search.." id="myInput" onKeyUp={filterFunction} />
+                        <a href="#about">About</a>
+                        <a href="#base">Base</a>
+                        <a href="#blog">Blog</a>
+                        <a href="#contact">Contact</a>
+                        <a href="#custom">Custom</a>
+                        <a href="#support">Support</a>
+                        <a href="#tools">Tools</a>
+                    </div>
+                </div>
             </div>
             <div className='watchlist-box'>
                 
